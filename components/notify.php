506559372dfe55c45efe3a40bd3fe66328024082
ingen <akbarzadeh.mojtaba@gmail.com>
2020-05-20 18:00:50 +0430

    اصلاح ارسال پیامک تایید

--- components/notify.php
+++ components/notify.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace  rabint\notify\components;
+namespace rabint\notify\components;
 
 use rabint\services\sms\sms;
 use common\models\User;
 use rabint\notify\models\Notification;
+
 //use ElephantIO\Client;
 //use ElephantIO\Engine\SocketIO\Version2X;
 
@@ -30,8 +31,40 @@ class notify extends \yii\base\Component
         }
     }
 
+    public function sendVerify($user_id, $token, $link = '', $params = [])
+    {
+
+        /* ------------------------------------------------------ */
+        $notify = new Notification();
+        $notify->user_id = $user_id;
+        $notify->media = isset($params['media']) ? $params['media'] : Notification::MEDIA_MUTE;
+        $notify->expire_at = !isset($params['expire_time']) ? NULL : time() + $params['expire_time'];
+        $notify->link = (!empty($link)) ? \yii\helpers\Url::to($link, TRUE) : NULL;
+        $notify->content = $token;
+        if ($notify->save(false)) {
+
+
+            //todo add meta and processVerify
+            //$this->processVerify($notify);
+
+            $user = User::findOne($notify->user_id);
+            $mobile = ($user->userProfile) ? $user->userProfile->cell : '';
+            $email = ($user->email) ? $user->email : '';
+            if ($notify->media & Notification::MEDIA_SMS) {
+                if ($mobile) {
+                    $res = sms::sendVerify($mobile, $token);
+                    $notify->send_status = $notify->send_status | Notification::MEDIA_SMS;
+                    $notify->save(false);
+                }
+            }
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
     /**
-     * 
+     *
      * @param Notification $notify
      * @return boolean
      */
